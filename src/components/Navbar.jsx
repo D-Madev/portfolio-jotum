5f942a1076992cc42fa392fc3022d754d1d5c2f9
D-Malex <3matias.sm@gmail.com>
2025-04-11 15:39:35 -0300

    feat(navbar): Agregando responsividad

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import './navbar.css';
 import logo from '../assets/logo-jotum-sf.png';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return(
     <nav className="navbar">
       <img src={logo} alt="logo-jotum"/>
-      <ul>
+
+      <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      <ul className={menuOpen ? 'open' : ''}>
         <li><a href="/Inicio">Inicio</a></li>
         <li><a href="/Servicios">Servicios</a></li>
         <li><a href="/Nosotros">Nosotros</a></li>
