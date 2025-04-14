cf0e55e0480f3fff5231c18d9c3026171d80eb76
D-Malex <3matias.sm@gmail.com>
2025-04-13 21:00:41 -0300

    feat(Benefits): Agrego nueva seccion, sin responsive

--- src/components/Benefit-card.jsx
+++ src/components/Benefit-card.jsx
@@ -3,9 +3,11 @@ import './Benefit-card.css'
 function BenefitCard({ img, title, description }) {
   return (
     <div className='benefit-card'>
-      <img src={img} alt={title} className='benefit-img' />
+      <div className="benefit-img-wrapper">
+        <img src={img} alt={title} className="benefit-img" />
+      </div>
       <h3 className='benefit-title'>{title}</h3>
-      {/*before*/}
+      <div className='card-decor-line'/>
       <p className='benefit-description'>{description}</p>
     </div>
   );
