145dc7cd007b9d72ea65c328f275cf2511f6b08a
D-Malex <3matias.sm@gmail.com>
2025-04-16 21:56:41 -0300

    feat(Testimonials): Agrego avatares y estrellas, junto a estilos

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,27 +1,31 @@
 import './Testimonials.css';
+import avatar1 from '../assets/avataaars.png'
+import avatar2 from '../assets/avataaars_1.png'
+import avatar3 from '../assets/avataaars_2.png'
+import avatar4 from '../assets/avataaars_3.png'
 import TestimonialsList from './Testimonials-list.jsx';
 
 const testimonialsData = [
   {
-    image: "",
+    image: avatar2,
     author: "Juan Pérez",
-    rate: 1,
+    rate: 5,
     content: "El servicio fue excepcional, superaron mis expectativas.",
   },
   {
-    image: "",
+    image: avatar1,
     author: "Ana Gomez",
-    rate: 2,
+    rate: 4,
     content: "Terrible una re locura mamita.",
   },
   {
-    image: "",
+    image: avatar3,
     author: "jose jsoe",
-    rate: 3,
+    rate: 5,
     content: "ai jesusito salvame de perderme esto.",
   },
   {
-    image: "",
+    image: avatar4,
     author: "La lorena",
     rate: 4,
     content: "OPA OPA jajaj terriblemisima ofertotona.",
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -3,10 +3,16 @@ import './Testimony.css'
 function Testimony({ image, author, rate, content }) {
   return(
     <article className='testimony'>
-      <img src={image} alt="profile photo" />
-      <h1>{author}</h1>
-      <p>{rate}</p>
-      <p>{content}</p>
+      <header className='testimony-header'>
+        <img src={image} alt={image} className='testimony-avatar'/>
+        <div className="testimony-info">
+          <h1>{author}</h1>
+          <p className="testimony-rate">
+            {Array.from({ length: rate }).map((_, i) => '★').join('')}
+          </p>
+        </div>
+      </header>
+      <p className='testimony-content'>{content}</p>
     </article>
   )
 }
