d543f2dc72a02535bcd4078979a33ea77ffe1ded
D-Malex <3matias.sm@gmail.com>
2025-04-22 20:51:33 -0300

    style(resume-aboutus): Cambio estetico

--- src/components/Resume-aboutus.jsx
+++ src/components/Resume-aboutus.jsx
@@ -1,28 +1,16 @@
 import './Resume-aboutus.css'
-import exampleMain from '../assets/Example-main.png'
-import exampleSubmain from '../assets/Example-submain.png'
-import exampleAdition from '../assets/Example-adition.png'
 
 function ResumeAboutUs() {
   return (
     <article className="resume-aboutus">
-      <section className='resume-aboutus-images'>
-        <img src={exampleMain} alt="example-main" className='img-main' />
-        <img src={exampleSubmain} alt="example-submain" className='img-sub' />
-        <img src={exampleAdition} alt="example-adition" className='img-add' />
-      </section>
       <section className='resume-aboutus-text'>
-        <h3>Un poco sobre nosotros</h3>
-        <p>
-          Jötum es una constructora nacional especializada en arquitectura e
-          ingeniería para viviendas unifamiliares y desarrollos a medida. Con
-          un enfoque innovador, combinamos diseño moderno, eficiencia y
-          precisión para crear espacios únicos que perduran en el tiempo.
-          Cada proyecto está pensado para transformar el espacio de
-          nuestros clientes en el hogar de sus sueños, donde funcionalidad y
-          estética se unen para ofrecer una experiencia de vida excepcional.
+        <h3 className="resume-aboutus-title">Un poco sobre nosotros</h3>
+        <p className="resume-aboutus-paragraph"> 
+        Jötum es una constructora nacional especializada en arquitectura e ingeniería para viviendas unifamiliares y desarrollos a medida. Nos dirigimos a quienes valoran una calidad de vida excepcional y entienden el diseño como una experiencia integral. Cada proyecto nace de la precisión, el diseño consciente y la atención al detalle, dando lugar a hogares únicos donde la estética se encuentra con la funcionalidad para crear espacios verdaderamente habitables, pensados para perdurar y disfrutarse en el día a día.
         </p>
-        <p className='resume-aboutus-quote'>“Less is more” (Menos es mas) - Ludwig Mies van der Rohe</p>
+        <p className='resume-aboutus-quote'>"La forma sigue a la función." - Walter Gropius</p>
+      </section>
+      <section className='resume-aboutus-stats'>
       </section>
     </article>
   );
