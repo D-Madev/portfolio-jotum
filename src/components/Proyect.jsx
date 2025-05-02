7ff12cd189ed674d60e03c33cfd9d8299a7888cd
D-Malex <3matias.sm@gmail.com>
2025-05-02 04:33:53 -0300

    style(proyects): Agrego sombra a las cards

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -3,20 +3,22 @@ import './Proyect.css'
 function Proyect({img, title, location, m2, state, description}) {
   return(
     <section className="proyect">
-      <img src={img} alt="Imagen de proyecto" />
-      <div className="proyect-content">
-        <div className="proyect-header">
+      <div className="proyect-img-container">
+        <img src={img} alt="Imagen de proyecto" />
+      </div>
+      <article className="proyect-content">
+        <header className="proyect-header">
           <p className="proyect-title"><b>{title}</b> {location}</p>
           <p className="proyect-state">{state}</p>
-        </div>
-        <div className='proyect-resume-bar'>
+        </header>
+        <body className='proyect-resume-bar'>
           <i className="fas fa-ruler-combined"></i>  {m2} m²
           <i className="fas fa-bed"></i> 1
           <i className="fas fa-bath"></i> 1
           <i className="fas fa-car"></i>
-        </div>
+        </body>
         <p className="proyect-description">{description}</p>
-      </div>
+      </article>
     </section>
   );
 }
