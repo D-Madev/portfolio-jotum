01f213cd3084a4bc9208f8eb2493f3628209d380
D-Malex <3matias.sm@gmail.com>
2025-05-25 16:29:49 -0300

    feat(service-card): cambio estilos y disposicion de las cards

--- src/components/Service-card.jsx
+++ src/components/Service-card.jsx
@@ -2,13 +2,15 @@ import './Service-card.css'
 
 export default function ServiceCard({ logo, title, description}) {
   return(
-    <article className="service-card">
-      <img src={logo} alt={logo} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <div className="button-container">
-        <button className="service-button">Ver más</button>
-      </div>
-    </article>
+     <div className="service-card-wrapper">
+      <article className="service-card">
+        <img src={logo} alt={logo} />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div className="button-container">
+          <button className="service-button">Ver más</button>
+        </div>
+      </article>
+    </div>
   )
 }
