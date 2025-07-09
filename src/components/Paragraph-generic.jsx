0617b5f6a244b475e535c59290cbd4ce5e71e610
D-Malex <3matias.sm@gmail.com>
2025-07-09 20:25:27 -0300

    style(paragraph-generic): cambio de color de fondo y de letra para evitar encandilacion del usuario

--- src/components/Paragraph-generic.jsx
+++ src/components/Paragraph-generic.jsx
@@ -1,14 +1,16 @@
 import './paragraph-generic.css';
 
-function paragraphGeneric({ title, text1, text2, text3 }) {
+function paragraphGeneric({ title, text1, text2, text3, style }) {
     return (
-      <section className="paragraph-generic">
-        <h1 className='paragraph-generic-title'>{title}</h1>
-        <div className='paragraph-generic-content'>
-          {text1 && <p>{text1}</p>}
-          {text2 && <p>{text2}</p>}
-          {text3 && <p>{text3}</p>}
-        </div>
+      <section className="paragraph-generic-background" style={style}>
+        <section className="paragraph-generic">
+          <h1 className='paragraph-generic-title'>{title}</h1>
+          <div className='paragraph-generic-content'>
+            {text1 && <p>{text1}</p>}
+            {text2 && <p>{text2}</p>}
+            {text3 && <p>{text3}</p>}
+          </div>
+        </section>
       </section>
     );
 }
