d218065d7058856475ad83e73505121f67e478fe
D-Malex <3matias.sm@gmail.com>
2025-08-10 17:32:17 -0300

    style(inicio): cambios esteticos en tamaños de letra y espacio entre componentes

--- src/components/Resume-aboutus.jsx
+++ src/components/Resume-aboutus.jsx
@@ -28,7 +28,7 @@ export default function ResumeAboutUs() {
   return (
     <article ref={ref} className="resume-aboutus">
       <section className='resume-aboutus-text'>
-        <h3 className="resume-aboutus-title">Un poco sobre nosotros</h3>
+        <h2 className="resume-aboutus-title">Un poco sobre nosotros</h2>
         <p className="resume-aboutus-paragraph"> 
         Jötum es una constructora nacional especializada en arquitectura e ingeniería para viviendas unifamiliares y desarrollos a medida. Nos dirigimos a quienes valoran una calidad de vida excepcional y entienden el diseño como una experiencia integral. Cada proyecto nace de la precisión, el diseño consciente y la atención al detalle, dando lugar a hogares únicos donde la estética se encuentra con la funcionalidad para crear espacios verdaderamente habitables, pensados para perdurar y disfrutarse en el día a día.
         </p>
