1512e35fee1e8fc1e5ef17ab2bc85be5571d9dbc
D-Malex <3matias.sm@gmail.com>
2025-08-25 12:05:49 -0300

    feat(paragraph-generic): agrego funcionalidad toggle para diseño responsive

--- src/components/Paragraph-generic.jsx
+++ src/components/Paragraph-generic.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import useNavbarStore from '../store/navbarStore';
 import './paragraph-generic.css';
 
-export default function paragraphGeneric({ title, text1, text2, text3, style }) {
-
+export default function paragraphGeneric({ title, text1, text2, text3,minSize=450, maxSize=1500, style }) {
+  const [isOpen, setIsOpen]   = useState(false);
+  const toggleOpen = () => setIsOpen(o => !o)
+  
   const { ref, inView } = useInView({
     threshold: 0.2,   // cuando el 20% aparezca
   });
@@ -18,10 +20,12 @@ export default function paragraphGeneric({ title, text1, text2, text3, style }) 
     <section ref={ ref } className="paragraph-generic-background" style={style}>
       <section className="paragraph-generic">
         <h1 className='paragraph-generic-title'>{title}</h1>
-        <div className='paragraph-generic-content'>
+        <div className={`paragraph-generic-content ${isOpen? `open` : ''}`} onClick={toggleOpen} style={{ height: isOpen ? `${maxSize}px` : `${minSize}px` }}>
           {text1 && <p>{text1}</p>}
-          {text2 && <p>{text2}</p>}
-          {text3 && <p>{text3}</p>}
+          <i className='fas fa-angle-up n-show-up'></i>
+          <i className='fas fa-angle-down n-show-down'></i>
+          {text2 && <p className={`${isOpen? 'open' : 'hide'}`}>{text2}</p>}
+          {text3 && <p className={`${isOpen? 'open' : 'hide'}`}>{text3}</p>}
         </div>
       </section>
     </section>
