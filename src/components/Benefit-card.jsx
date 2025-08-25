dcff6256555dfaa458c0a2d836f251dfbfe5af96
D-Malex <3matias.sm@gmail.com>
2025-08-25 10:40:28 -0300

    feat(benefit-card): funcionalidad toggle para las cards responsive

--- src/components/Benefit-card.jsx
+++ src/components/Benefit-card.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import './Benefit-card.css'
 
 function BenefitCard({ img, title, description }) {
+  const [isOpen, setIsOpen]   = useState(false);
+
+  const toggleOpen = () => setIsOpen(o => !o);
+  
   return (
-    <div className='benefit-card'>
+    <div className={`benefit-card ${isOpen ? 'open' : ''}`} onClick={toggleOpen}>
       <div className="benefit-img-wrapper">
         <img src={img} alt={title} className="benefit-img"/>
       </div>
       <h3 className='benefit-title'>{title}</h3>
       <div className='card-decor-line'/>
+      <i className='fas fa-angle-up b-show-up'></i>
+      <i className='fas fa-angle-down b-show-down'></i>
       <p className='benefit-description'>{description}</p>
     </div>
   );
